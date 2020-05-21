70f3e838214af58d4e91bcc6346b1ee527c65319
LUNKUS <sigitas@lunkus.com>
2020-05-21 07:34:26 +0300

    Create file and fead from it

--- src/com/sigis/Main.java
+++ src/com/sigis/Main.java
@@ -1,16 +1,38 @@
 package com.sigis;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        // Create file
+        File createFile = new File("src/cFile.txt");
+
+        try(FileWriter fw = new FileWriter(createFile)) {
+
+            fw.write("This is the text to be written into the given file ...");
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        // Read from file
+        FileReader fr = new FileReader(createFile);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        System.out.println("----=====-----");
+
+        // Pointing to existing file
         File file = new File("src/Test.txt");
 
+        // Read from console
         try(FileInputStream fis = new FileInputStream(file)) {
 
             System.out.println("Total size in bytes: " + fis.available());
