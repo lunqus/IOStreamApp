e66e94310c8495adb5818279701015733af4c610
LUNKUS <sigitas@lunkus.com>
2020-05-21 07:19:01 +0300

    IO read from file and display into console

--- src/com/sigis/Main.java
+++ src/com/sigis/Main.java
@@ -1,20 +1,38 @@
 package com.sigis;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        byte[] data = new byte[11];
+        File file = new File("src/Test.txt");
 
-        System.out.println(" Enter some characters: ");
-        System.in.read(data);
+        try(FileInputStream fis = new FileInputStream(file)) {
 
-        System.out.println(" You entered: ");
+            System.out.println("Total size in bytes: " + fis.available());
 
-        for (int i = 0; i < data.length; i++) {
-            System.out.print((char)data[i]);
+            int content;
+            while ((content = fis.read()) != -1) {
+                System.out.print((char) content);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+//        byte[] data = new byte[11];
+//
+//        System.out.println(" Enter some characters: ");
+//        System.in.read(data);
+//
+//        System.out.println(" You entered: ");
+//
+//        for (int i = 0; i < data.length; i++) {
+//            System.out.print((char)data[i]);
+//        }
     }
 }
