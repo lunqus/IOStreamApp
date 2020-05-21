e61b9905f7d24de83fa6bac79f4eeced413e95ef
LUNKUS <sigitas@lunkus.com>
2020-05-21 06:38:08 +0300

    IO read info and display those in console

--- src/com/sigis/Main.java
+++ src/com/sigis/Main.java
@@ -1,8 +1,20 @@
 package com.sigis;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        byte[] data = new byte[11];
+
+        System.out.println(" Enter some characters: ");
+        System.in.read(data);
+
+        System.out.println(" You entered: ");
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.print((char)data[i]);
+        }
     }
 }
